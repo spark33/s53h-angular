b3b51e4a46512137d90136b784b0f3d6a576d172
Sean Park <seanpark@Seans-MBP-3.wv.cc.cmu.edu>
2016-12-18 03:22:28 -0500

    initial commit

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
+app.set('views', __dirname + '/views'); // general config
+app.set('view engine', 'html');
+
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/app'));
 
